6043e8ce71322f34a2bd6ce157326f7b915c8514
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-05-05 02:42:38 -0400

    second push

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 'use strict';
 
-// console log the current time on page load -
 
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 console.log(today, time);
 
 document.querySelector('.clock-display').textContent = time;
 
+setTimeout(function () {
+    location.reload();
+}, 1000);
 
